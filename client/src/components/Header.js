91e3c66086c3e7aaf14c08ab2bdc9c13050a3780
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-09-01 09:43:13 -0500

    added npm run dev script and added basic styling

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import { NavLink} from 'react-router-dom';
+import logo from '../images/GAMRx10.png'
+
+function Header() {
+  return (
+    <header>
+        <img id="logoPic" src={logo} alt="logo" />
+         <nav>
+           <NavLink to="/" >Home</NavLink>
+           <NavLink to="/auth-form">Register/Login</NavLink>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,10 @@
+
+function Landing() {
+  return (
+    <div>
+        <h1>Welcome Guest</h1>
+    </div>
+  )
+}
+
+export default Landing;
