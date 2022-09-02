8c262ce5cc21b123390fcb2dc99fa05435e92cb6
Minotaurius <brandonslanger@gmail.com>
2022-09-02 14:58:06 -0500

    Fixed profile error

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import Profile from './Profile'
-=======
-
->>>>>>> fb6579ae1818050db93031eedd87298ba6dcb8c6
 function Landing() {
   return (
     <div>
         <h1>Welcome Guest</h1>
-<<<<<<< HEAD
-        <Profile />
-=======
->>>>>>> fb6579ae1818050db93031eedd87298ba6dcb8c6
     </div>
   )
 }
