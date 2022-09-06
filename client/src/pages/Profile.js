1d6ca2a5456d108dc07c3e4fc39c0194a720ae5e
Minotaurius <brandonslanger@gmail.com>
2022-09-05 19:13:17 -0500

    Switching branches trying a thing

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,36 +2,36 @@ import { useQuery } from '@apollo/client';
 import { PROFILE_QUERY } from '../utils/queries';
 
 
-// function Profile(props) {
-//     const { loading, error, data } = useQuery(PROFILE_QUERY)
-
-//     return(
-//         <div>
-//             {error && <p className="error">{error.message}</p>}
-
-//             {loading && <p>Loading user profile...</p>}
-
-//             {data && (
-//                 <span>
-//                     {data.getUsers.map(user => (
-//                     <h3 key={user._id}>{user.gamerTag}</h3>
-//                     ))}
-//                 </span>
-//             )}
-//         </div>
-
-//   )
-// }
-
 function Profile(props) {
-    const { user } = this.props;
+    const { loading, error, data } = useQuery(PROFILE_QUERY)
+
     return(
         <div>
-            <h1>{user.email}</h1>
-            {/* <h3>{props.user.faveConsole}</h3> */}
+            {error && <p className="error">{error.message}</p>}
+
+            {loading && <p>Loading user profile...</p>}
+
+            {data && (
+                <span>
+                    {data.getUsers.map(user => (
+                    <h3 key={user._id}>{user.gamerTag}</h3>
+                    ))}
+                </span>
+            )}
         </div>
-    )
+
+  )
 }
+
+// function Profile(props) {
+//     const { user } = this.props;
+//     return(
+//         <div>
+//             <h1>{user.email}</h1>
+//             {/* <h3>{props.user.faveConsole}</h3> */}
+//         </div>
+//     )
+// }
 
 export default Profile;
 
