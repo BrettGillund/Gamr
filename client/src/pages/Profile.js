0f1da774570b45b20df5c6959132bf94369788a8
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-09-05 22:03:14 -0500

    my changes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -22,33 +22,6 @@ import { PROFILE_QUERY } from "../utils/queries";
 // }
 
 function Profile() {
-<<<<<<< HEAD
-    const { loading, error, data } = useQuery(PROFILE_QUERY)
-    let userInfo = JSON.parse(localStorage.getItem('user'))
-
-    return (
-        <div>
-            {error && <p className="error">{error.message}</p>}
-
-            {loading && <p>Loading user profile...</p>}
-
-            {data && (
-                <span>
-                    {data.getUsers.map((user, index) => {
-                        if(userInfo.email === user.email) {
-                            return <div key={index}>
-                            <h3>{user.gamerTag}</h3>
-                            <h3>{user.faveConsole}</h3>
-                            <h3>{user.library[0].game}</h3>
-                            </div>
-                        }
-})}
-                </span>
-            )}
-        </div>
-
-    )
-=======
 
   const { loading, error, data } = useQuery(PROFILE_QUERY);
 
@@ -83,7 +56,6 @@ function Profile() {
       </div> */}
     </div>
   );
->>>>>>> e885b9e493a67bca3a5d436722534ca53f99643d
 }
 
 // function Profile(props) {
