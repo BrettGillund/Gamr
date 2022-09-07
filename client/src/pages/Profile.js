1401f963f49a7b9defe4339937e520736201567b
JamesB <James.buchmann@gmail.com>
2022-09-07 10:21:55 -0500

    games added to user and displaying on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,63 +1,62 @@
 import { useQuery } from "@apollo/client";
 import { PROFILE_QUERY } from "../utils/queries";
 
-// function Profile() {
-// let userInfo = JSON.parse(localStorage.getItem('user'))
+import GameForm from "../components/GameForm";
 
-//     console.log(userInfo)
-
-//     return (
-//         <div className="game-container">
-//             <div>
-//                 <div className="game-card">
-//                     <p></p>
-//                     <p></p>
-//                     <p></p>
-//                 </div>
-//             </div>
-
-//         </div>
-
-//     )
-// }
 
 function Profile() {
   const { loading, error, data } = useQuery(PROFILE_QUERY);
   let userInfo = JSON.parse(localStorage.getItem("user"));
+  console.log(data);
+
+
 
   return (
-    <div>
-      {error && <p className="error">{error.message}</p>}
+    <div className="profile-main">
+      <div>
+        {error && <p className="error">{error.message}</p>}
 
-      {loading && <p>Loading user profile...</p>}
+        {loading && <p>Loading user profile...</p>}
 
-      {data && (
-        <span>
-          {data.getUsers.map((user, index) => {
-            if (userInfo.email === user.email) {
-              return (
-                <div key={index}>
-                  <h3>{user.gamerTag}</h3>
-                  <h3>{user.faveConsole}</h3>
-                  <h3>{user.library[0].game}</h3>
-                </div>
-              );
-            }
-          })}
-        </span>
-      )}
-    </div>
+        {data && (
+          <span>
+            {data.getUsers.map((user, index) => {
+              if (userInfo.email === user.email) {
+              let userLib = user.library
+              console.log(userLib)
+                return (
+                  
+                  <div key={index}>
+                    <div>
+                      <h3>{user.gamerTag}</h3>
+                      <h3>{user.faveConsole}</h3>
+                    <div className="gameList">
+
+                      {user.library.map((game, index) => {
+                        return (
+                          <div  key={index}>
+                            <h1>
+                               {game.game}
+                            </h1>
+
+                          </div>
+                        )
+                      })}
+                    </div>
+                    
+                    </div>
+
+                    <GameForm {...user}/>
+                  </div>
+                );
+              }
+            })}
+          </span>
+        )}
+      </div>
+      <div>
+      </div>
+  </div>
   );
 }
-
-// function Profile(props) {
-//     const { user } = this.props;
-//     return(
-//         <div>
-//             <h1>{user.email}</h1>
-//             {/* <h3>{props.user.faveConsole}</h3> */}
-//         </div>
-//     )
-// }
-
 export default Profile;
